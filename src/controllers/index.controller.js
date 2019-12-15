e7e48bed9c6cc9173a323f73c23d7d2521c9457e
lvhquan <lvhquan0110@gmail.com>
2019-12-16 00:57:01 +0700

    crud for phone label

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -2,10 +2,12 @@ import authController from "./auth.controller";
 import productController from "./product.controller";
 import commentController from "./comment.controller";
 import userController from "./user.controller";
+import labelController from "./label.controller";
 
 module.exports = {
     authController,
     productController,
     commentController,
-    userController
+    userController,
+    labelController
 }
--- src/services/index.service.js
+++ src/services/index.service.js
@@ -1,9 +1,11 @@
 import userService from "./user.service";
 import productService from "./product.service";
 import commentService from "./comment.service";
+import labelService from "./label.service";
 
 module.exports = {
     userService,
     productService,
-    commentService
+    commentService,
+    labelService
 }
